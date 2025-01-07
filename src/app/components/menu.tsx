eb7b692fa5ed053978ff1e6a9f0f0ee913ab7cf1
Carlos Eduardo <67555079+caduhandrade@users.noreply.github.com>
2025-01-07 11:01:58 -0300

    Update menu.tsx

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -2,7 +2,7 @@ export default function Menu() {
   return (
     <header className="container flex items-center justify-between gap-8 py-4">
       <a href="./">
-        <img src="assets/forest.svg" alt="Forest" />
+        <Image src="assets/forest.svg" alt="Forest" />
       </a>
       <nav>
         <ul className="text-white text-xl flex flex-wrap gap-8">
